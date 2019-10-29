114310c9ed597ebeeed9d7f69f0caa46c7cb0d43
OmariM <omatthews@g.hmc.edu>
2019-10-29 16:35:49 -0700

    Added question class for survey

--- MoodMusic/MoodMusic/Models/Question.swift
+++ MoodMusic/MoodMusic/Models/Question.swift
@@ -6,12 +6,27 @@
 //  Copyright © 2019 Vibe. All rights reserved.
 //
 
-// label
-// list of choices
 
-// initialize with a string as the label and a list of string that are the choices
-// ex: ["pop", "edm", "classical"]
 
 class Question {
+
+    // label
+    let questionText : String
+    
+    // list of choices
+    let option1 : String
+    let option2 : String
+    let option3 : String
+    let option4 : String
+    
+    // initialize with a string as the label and a list of string that are the choices
+    // ex: ["pop", "edm", "classical"]
+    init(text: String, options: Array<String>) {
+        questionText = text;
+        self.option1 = options[0]
+        self.option2 = options[1]
+        self.option3 = options[2]
+        self.option4 = options[3]
+    }
     
 }
