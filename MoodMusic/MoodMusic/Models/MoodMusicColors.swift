50a72450becc924b745f3ad7d45811324f3c0a55
OmariM <omatthews@g.hmc.edu>
2019-11-19 00:46:00 -0800

    added spotify button touch up/down colors and made functions that return colors

---
+++ MoodMusic/MoodMusic/Models/MoodMusicColors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors Used.swift
+//  MoodMusic
+//
+//  Created by Nina on 11/18/19.
+//  Copyright © 2019 Vibe. All rights reserved.
+//
+
+import UIKit
+
+// SURVEY BACKGROUND
+func moody_backgroundPurple() -> UIColor {
+    return UIColor(red: 28.05/255.0, green: 20.4/255.0, blue: 99.45/255.0, alpha: 1.0)
+}
+
+// BUTTONS
+func moody_buttonPurple() -> UIColor {
+    return UIColor(red: 163.2/255.0, green: 155.55/255.0, blue: 255.0/255.0, alpha: 1.0)
+}
+
+func moody_spotifyButtonPurple() -> UIColor {
+    return UIColor(red: 108.0/255.0, green: 86.0/255.0, blue: 151.0/255.0, alpha: 1.0)
+}
+
+func moody_spotifyButtonDarkPurple() -> UIColor {
+    return UIColor(red: 76.0/255.0, green: 61.0/255.0, blue: 108.0/255.0, alpha: 1.0)
+}
